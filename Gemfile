4738c18ceb1a0d49edc116ffd3d51305e4d83138
Adam Miribyan <kreditmarket.com@gmail.com>
2011-05-02 02:46:50 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'mysql2', '0.2.7'
+gem 'unicode'
+
+gem 'sass'
+gem 'simple_form'
+gem 'russian'
+
+gem 'mini_magick'
+gem 'carrierwave'
+
+gem 'jquery-rails', '~>0.2'
+gem 'rails3-jquery-autocomplete'
+gem 'jeditable-rails'
+
+group :development do
+  gem 'capistrano'
+  gem 'thin'
+end
+
+group :development, :test do  
+  gem 'mocha'
+  gem 'factory_girl_rails'
+  gem 'autotest-standalone'
+  gem 'autotest-growl'
+  gem 'mynyml-redgreen'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inmemoria::Application.config.secret_token = '995f42d6afead35f7c395d227a1494c20a87a9490b93825876409d2cb5478d1ea596ca4f7ca39ffd5cfb0b9a42c3896f2d590ab3661a30b340890138d2677c33'
